47c7107c062d3b1e1128429e8c33b210404fbe93
大下慎平 <one1218all38@icloud.com>
2024-09-23 09:00:25 +0900

    linter微修正

--- frontend/src/api/models.ts
+++ frontend/src/api/models.ts
@@ -1,76 +1,76 @@
 export enum Approaches {
-	ReadRetrieveRead = "rrr",
-	Read = "r",
+    ReadRetrieveRead = "rrr",
+    Read = "r",
 }
 
 export type AskRequestOverrides = {
-	gptModel?: string;
-	semanticRanker?: boolean;
-	semanticCaptions?: boolean;
-	excludeCategory?: string;
-	top?: number;
-	temperature?: string;
-	promptTemplate?: string;
-	promptTemplatePrefix?: string;
-	promptTemplateSuffix?: string;
+    gptModel?: string;
+    semanticRanker?: boolean;
+    semanticCaptions?: boolean;
+    excludeCategory?: string;
+    top?: number;
+    temperature?: string;
+    promptTemplate?: string;
+    promptTemplatePrefix?: string;
+    promptTemplateSuffix?: string;
 };
 
 export type AskRequest = {
-	question: string;
-	approach: Approaches;
-	overrides?: AskRequestOverrides;
+    question: string;
+    approach: Approaches;
+    overrides?: AskRequestOverrides;
 };
 
 export type AskResponse = {
-	answer: string;
-	thoughts: string | null;
-	data_points: string[];
-	error?: string;
+    answer: string;
+    thoughts: string | null;
+    data_points: string[];
+    error?: string;
 };
 
 export type ChatTurn = {
-	user: string;
-	bot?: string;
+    user: string;
+    bot?: string;
 };
 
 export type ChatRequest = {
-	history: ChatTurn[];
-	approach: Approaches;
-	overrides?: AskRequestOverrides;
+    history: ChatTurn[];
+    approach: Approaches;
+    overrides?: AskRequestOverrides;
 };
 
 export type GptChatTurn = {
-	user: string;
-	assistant?: string;
+    user: string;
+    assistant?: string;
 };
 
 export type GptChatRequest = {
-	history: GptChatTurn[];
-	approach: Approaches;
-	overrides?: GptRequestOverrides;
+    history: GptChatTurn[];
+    approach: Approaches;
+    overrides?: GptRequestOverrides;
 };
 
 export type GptRequestOverrides = {
-	gptModel?: string;
-	temperature?: string;
-	systemPrompt?: string;
+    gptModel?: string;
+    temperature?: string;
+    systemPrompt?: string;
 };
 
 export type ChatResponse = {
-	answer: string;
-	error?: string;
+    answer: string;
+    error?: string;
 };
 
 export type Claim = {
-	typ: string;
-	val: string;
+    typ: string;
+    val: string;
 };
 
 export type AccessToken = {
-	access_token: string;
-	expires_on: string;
-	id_token: string;
-	provider_name: string;
-	user_claims: Claim[];
-	user_id: string;
+    access_token: string;
+    expires_on: string;
+    id_token: string;
+    provider_name: string;
+    user_claims: Claim[];
+    user_id: string;
 };
